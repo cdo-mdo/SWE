91943590848054f1ef6e99c8f7d2605b52cb860d
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-03-08 15:39:05 -0600

    refactor code to remove boolean parameter

--- assignment-07b/MortgageProject/src/main/java/org/edu/miu/cs/cs425/MortgageCalculator.java
+++ assignment-07b/MortgageProject/src/main/java/org/edu/miu/cs/cs425/MortgageCalculator.java
@@ -5,8 +5,25 @@ import java.time.Period;
 
 public class MortgageCalculator {
 
-	public double computeMaxMortgage(int yearOfBirt, int month, int day, double monthlyIncome, boolean married, double monthlyIncomePartner, String profession) {
-		double result=0;
+	public double computeMaxMortgageSingle(int yearOfBirt, int month, int day, double monthlyIncome, String profession) {
+		if (ageOf(yearOfBirt, month, day) < 18) {
+			return 0;
+		}
+
+		return computeMaxMortgage(monthlyIncome, profession);
+	}
+
+	public double computeMaxMortgageMarried(int yearOfBirt, int month, int day, double monthlyIncome, double monthlyIncomePartner, String profession) {
+		if (ageOf(yearOfBirt, month, day) < 18) {
+			return 0;
+		}
+
+ 		double totalIncome = monthlyIncome + monthlyIncomePartner * 0.94;
+
+		return computeMaxMortgage(totalIncome, profession);
+	}
+
+	private int ageOf(int yearOfBirt, int month, int day) {
 		//calculate age
 		LocalDate today = LocalDate.now();                          //Today's date
 		LocalDate birthday = LocalDate.of(yearOfBirt, month, day);  //Birth date
@@ -15,29 +32,25 @@ public class MortgageCalculator {
 		int age = p.getYears();
 		System.out.println(age);
 
-		if (age < 18) {
-			result = 0;
-			return result;
-		}
+		return age;
+	}
 
-		double totalIncome = monthlyIncome;
-		if (married) {
-			totalIncome = monthlyIncome + monthlyIncomePartner * 0.94;
-		}
+	private double computeMaxMortgage(double monthlyIncome, String profession) {
+		double result=0;
 
-		if (2000<=totalIncome && totalIncome<3000) {
+		if (2000<=monthlyIncome && monthlyIncome<3000) {
 			if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 160000;
 			if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 120000;
 			if (profession.equals("Department head") || profession.equals("Professor") ) result = 220000;
 			return result;
 		}
-		if (3000<=totalIncome && totalIncome<5000) {
+		if (3000<=monthlyIncome && monthlyIncome<5000) {
 			if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 180000;
 			if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 140000;
 			if (profession.equals("Department head") || profession.equals("Professor") ) result = 250000;
 			return result;
 		}
-		if (5000<=totalIncome) {
+		if (5000<=monthlyIncome) {
 			if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 220000;
 			if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 160000;
 			if (profession.equals("Department head") || profession.equals("Professor") ) result = 280000;
--- assignment-07b/MortgageProject/src/test/java/MortgageTest.java
+++ assignment-07b/MortgageProject/src/test/java/MortgageTest.java
@@ -8,28 +8,28 @@ public class MortgageTest {
 	@Test
 	public void test1() {
 		MortgageCalculator mc = new MortgageCalculator();
-		double result = mc.computeMaxMortgage(1967, 5, 2, 3000, false, 0, "Architect");
+		double result = mc.computeMaxMortgageSingle(1967, 5, 2, 3000, "Architect");
 	    assertEquals(180000, result, 0.001);
 	}
 	
 	@Test
 	public void test2() {
 		MortgageCalculator mc = new MortgageCalculator();
-		double result = mc.computeMaxMortgage(2001, 5, 2, 3000, false, 0, "Architect");
+		double result = mc.computeMaxMortgageSingle(2001, 5, 2, 3000, "Architect");
 	    assertEquals(180000, result, 0.001);
 	}
 	
 	@Test
 	public void test3() {
 		MortgageCalculator mc = new MortgageCalculator();
-		double result = mc.computeMaxMortgage(1977, 12, 2, 4000, true, 2000, "Developer");
+		double result = mc.computeMaxMortgageMarried(1977, 12, 2, 4000, 2000, "Developer");
 	    assertEquals(220000, result, 0.001);
 	}
 	
 	@Test
 	public void test4() {
 		MortgageCalculator mc = new MortgageCalculator();
-		double result = mc.computeMaxMortgage(1954, 5, 12, 5500, false, 0, "Professor");
+		double result = mc.computeMaxMortgageMarried(1954, 5, 12, 5500, 0, "Professor");
 	    assertEquals(280000, result, 0.001);
 	}
 
