c697b6c44be94d9cb2e841fe0d14a49cd7eee274
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-03-09 13:10:09 -0500

    format code

--- assignment-07b/ShoppingCart/src/products/Product.java
+++ assignment-07b/ShoppingCart/src/products/Product.java
@@ -1,38 +1,39 @@
 package products;
+
 public class Product {
-	String productnumber;
-	double price;
-	String description;
+    String productnumber;
+    double price;
+    String description;
 
-	public Product(String productnumber, double price, String description) {
-		super();
-		this.productnumber = productnumber;
-		this.price = price;
-		this.description = description;
-	}
+    public Product(String productnumber, double price, String description) {
+        super();
+        this.productnumber = productnumber;
+        this.price = price;
+        this.description = description;
+    }
 
-	public String getProductnumber() {
-		return productnumber;
-	}
+    public String getProductnumber() {
+        return productnumber;
+    }
 
-	public void setProductnumber(String productnumber) {
-		this.productnumber = productnumber;
-	}
+    public void setProductnumber(String productnumber) {
+        this.productnumber = productnumber;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public void setPrice(double price) {
-		this.price = price;
-	}
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
--- assignment-07b/ShoppingCart/src/shoppingcart/CartLine.java
+++ assignment-07b/ShoppingCart/src/shoppingcart/CartLine.java
@@ -1,24 +1,25 @@
 package shoppingcart;
+
 import products.Product;
 
 public class CartLine {
-	int q;
-	Product p;
+    int q;
+    Product p;
 
-	public Product getP() {
-		return p;
-	}
+    public Product getP() {
+        return p;
+    }
 
-	public void setP(Product p) {
-		this.p = p;
-	}
+    public void setP(Product p) {
+        this.p = p;
+    }
 
-	public int getQ() {
-		return q;
-	}
+    public int getQ() {
+        return q;
+    }
 
-	public void setQ(int q) {
-		this.q = q;
-	}
+    public void setQ(int q) {
+        this.q = q;
+    }
 
 }
