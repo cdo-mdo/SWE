f60e50abc20026da2f49c8ea7820e1a41a5666e0
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-03-09 09:44:20 -0500

    add test under 18

--- assignment-07b/MortgageProject/src/test/java/MortgageTest.java
+++ assignment-07b/MortgageProject/src/test/java/MortgageTest.java
@@ -3,10 +3,20 @@ import org.junit.Test;
 
 import org.edu.miu.cs.cs425.MortgageCalculator;
 
+import java.time.LocalDate;
+
 public class MortgageTest {
 
 	@Test
-	public void test1() {
+	public void testUnder18() {
+		LocalDate time17YearsAgo = LocalDate.now().minusYears(17);
+		MortgageCalculator mc = new MortgageCalculator();
+		double result = mc.computeMaxMortgageSingle(time17YearsAgo.getYear(), time17YearsAgo.getMonthValue(), time17YearsAgo.getDayOfMonth(), 3000, "Architect");
+		assertEquals(0, result, 0.001);
+	}
+
+	@Test
+	public void testOver18SingleArchitect() {
 		MortgageCalculator mc = new MortgageCalculator();
 		double result = mc.computeMaxMortgageSingle(1967, 5, 2, 3000, "Architect");
 	    assertEquals(180000, result, 0.001);
